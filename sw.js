42ea28dac15df7779c950d483556c20c71b12629
bernhardsteiert <bernhard.steiert@googlemail.com>
2026-06-02 17:32:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,37 +1,20 @@
 const CACHE = 'lendo-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
 });
 
 self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-
-  // Firebase Requests: Network first, kein Cache
-  if (url.hostname.includes('firebasedatabase')) {
-    e.respondWith(
-      fetch(e.request).catch(() => new Response('null', { headers: { 'Content-Type': 'application/json' } }))
-    );
+  if (e.request.url.includes('firebasedatabase')) {
+    e.respondWith(fetch(e.request).catch(() => new Response('null', {headers:{'Content-Type':'application/json'}})));
     return;
   }
-
-  // App-Assets: Cache first, dann Network
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(resp => {
-      const clone = resp.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return resp;
-    }))
-  );
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
 });
